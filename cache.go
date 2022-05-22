102fe90c9add9aff528e00e3553e838b96a6cc6c
devandscorp <artemda4@gmail.com>
2022-05-22 13:55:04 +0300

    feat: add solution

--- cache.go
+++ cache.go
@@ -2,22 +2,55 @@ package cache
 
 import "time"
 
+type CacheValue struct {
+	value string
+	useDeadline bool
+	deadline time.Time
+}
 type Cache struct {
+	cacheMap map[string]CacheValue
 }
 
 func NewCache() Cache {
-	return Cache{}
+	return Cache{
+		cacheMap: map[string]CacheValue{},
+	}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cache Cache) Get(key string) (string, bool) {
+	cachedValue, ok := cache.cacheMap[key]
+	if (!ok) {
+		return "", false
+	}
+	if (cachedValue.useDeadline && cachedValue.deadline.Before(time.Now())) {
+		return "", false
+	}
+	return cachedValue.value, true
 }
 
-func (receiver) Put(key, value string) {
+func (cache Cache) Put(key, value string) {
+	cache.cacheMap[key] = CacheValue{
+		value: value,
+		useDeadline: false,
+		deadline: time.Time{},
+	}
 }
 
-func (receiver) Keys() []string {
+func (cache Cache) Keys() []string {
+	keys := []string{}
+
+	for key := range cache.cacheMap {
+		if (cache.cacheMap[key].useDeadline && cache.cacheMap[key].deadline.After(time.Now())) {
+			keys = append(keys, key)
+		}
+	}
+	return keys
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache Cache) PutTill(key, value string, deadline time.Time) {
+	cache.cacheMap[key] = CacheValue{
+		value: value,
+		useDeadline: true,
+		deadline: deadline,
+	}
 }
